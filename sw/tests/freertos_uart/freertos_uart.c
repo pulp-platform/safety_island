f123e9ddf0f2b3b35ecc0999a2b89753484acaed
aottaviano <aottaviano@iis.ee.ethz.ch>
2023-03-23 22:35:50 +0100

    sw: Add sw/ folder with runtime and rtos tests

---
+++ sw/tests/freertos_uart/freertos_uart.c
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2020 ETH Zurich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Author: Robert Balas (balasr@iis.ee.ethz.ch)
+ */
+
+/*
+ * A minimal pmsis example. Mainly used to test if we can link against pmsis.
+ */
+
+/* FreeRTOS kernel includes. */
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* c stdlib */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <inttypes.h>
+
+/* system includes */
+#include "system.h"
+#include "timer_irq.h"
+#include "fll.h"
+#include "irq.h"
+#include "gpio.h"
+
+/* pmsis */
+#include "target.h"
+#include "os.h"
+
+void vApplicationMallocFailedHook(void);
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
+void vApplicationTickHook(void);
+
+char hello[40];
+
+void helloworld(void)
+{
+	printf("Entering main controller\n");
+	uint32_t core_id = pi_core_id(), cluster_id = pi_cluster_id();
+
+	sprintf(hello, "[%"PRIu32" %"PRIu32"] Hello World!\n", cluster_id, core_id);
+	puts(hello);
+
+	pmsis_exit(EXIT_SUCCESS);
+}
+
+/* Program Entry. */
+int main(void)
+{
+	/* Init board hardware. */
+	system_init();
+
+	printf("\n\n\t *** FreeRTOS HelloWorld *** \n\n");
+	return pmsis_kickoff((void *) helloworld);
+}
+
+
+void vApplicationMallocFailedHook(void)
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	taskDISABLE_INTERRUPTS();
+	printf("error: application malloc failed\n");
+	__asm volatile("ebreak");
+	for (;;)
+		;
+}
+
+void vApplicationIdleHook(void)
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
+{
+	(void)pcTaskName;
+	(void)pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	printf("error: stack overflow\n");
+	__asm volatile("ebreak");
+	for (;;)
+		;
+}
+
+void vApplicationTickHook(void)
+{
+}
+
---
+++ sw/tests/runtime_shared/include/clic.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2021 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Author: Robert Balas (balasr@iis.ee.ethz.ch)
+ */
+
+#ifndef __CLIC_H__
+#define __CLIC_H__
+
+/* Number of interrupt sources */
+/* taken from target configuration */
+/* #define CLIC_PARAM_NUM_SRC 256 */
+
+/* Number of interrupt control bits */
+/* taken from target configuration */
+/* #define CLIC_PARAM_CLIC_INT_CTL_BITS 8 */
+
+/* Register width */
+#define CLIC_PARAM_REG_WIDTH 32
+
+/* CLIC Configuration */
+#define CLIC_CLICCFG_REG_OFFSET	 0x0
+#define CLIC_CLICCFG_NVBITS_BIT	 0
+#define CLIC_CLICCFG_NLBITS_MASK 0xf
+#define CLIC_CLICCFG_NLBITS_OFFSET 1
+#define CLIC_CLICCFG_NMBITS_MASK   0x3
+#define CLIC_CLICCFG_NMBITS_OFFSET 5
+
+/* CLIC Information */
+#define CLIC_CLICINFO_REG_OFFSET	   0x4
+#define CLIC_CLICINFO_NUM_INTERRUPT_MASK   0x1fff
+#define CLIC_CLICINFO_NUM_INTERRUPT_OFFSET 0
+
+#define CLIC_CLICINFO_VERSION_MASK   0xff
+#define CLIC_CLICINFO_VERSION_OFFSET 13
+
+#define CLIC_CLICINFO_CLICINTCTLBITS_MASK   0xf
+#define CLIC_CLICINFO_CLICINTCTLBITS_OFFSET 21
+
+#define CLIC_CLICINFO_NUM_TRIGGER_MASK	 0x3f
+#define CLIC_CLICINFO_NUM_TRIGGER_OFFSET 25
+
+/* CLIC enable mnxti irq forwarding logic */
+#define CLIC_CLICXNXTICONF_REG_OFFSET 0x8
+#define CLIC_CLICXNXTICONF_CLICXNXTICONF_BIT 0
+
+/* CLIC interrupt id pending */
+#define CLIC_CLICINTIP_REG_OFFSET(id) (0x1000 + 0x10 * id)
+#define CLIC_CLICINTIP_CLICINTIP_BIT  0
+
+/* CLIC interrupt id enable */
+#define CLIC_CLICINTIE_REG_OFFSET(id) (0x1004 + 0x10 * id)
+#define CLIC_CLICINTIE_CLICINTIE_BIT  0
+
+/* CLIC interrupt id attributes */
+#define CLIC_CLICINTATTR_REG_OFFSET(id) (0x1008 + 0x10 * id)
+#define CLIC_CLICINTATTR_SHV_BIT	0
+#define CLIC_CLICINTATTR_TRIG_MASK	0x3
+#define CLIC_CLICINTATTR_TRIG_OFFSET	1
+#define CLIC_CLICINTATTR_MODE_MASK	0x3
+#define CLIC_CLICINTATTR_MODE_OFFSET	6
+
+#ifndef __ASSEMBLER__
+enum clic_trig {
+	CLIC_TRIG_LEVEL = 0,
+	CLIC_TRIG_EDGE = 1,
+	CLIC_TRIG_POSITIVE = 0 << 1,
+	CLIC_TRIG_NEGATIVE = 1 << 1
+};
+#endif
+
+/* CLIC interrupt id control */
+#define CLIC_CLICINTCTL_REG_OFFSET(id)	  (0x100c + 0x10 * id)
+#define CLIC_CLICINTCTL_CLICINTCTL_MASK	  0xff
+#define CLIC_CLICINTCTL_CLICINTCTL_OFFSET 0
+
+#define CSR_MXNTI_ID       0x345
+#define MIE                8
+
+#endif /* __CLIC_H__ */
