874a1ef8160d471b16eeb1807c74918e87e26654
Michael Rogenmoser <michael@rogenmoser.us>
2023-11-14 15:42:43 +0100

    Carfield Adressability test

--- sw/tests/runtime_shared/include/io.h
+++ sw/tests/runtime_shared/include/io.h
@@ -51,13 +51,13 @@ static inline void writew(uint32_t val, uintptr_t addr)
 		     : "memory");
 }
 
-static inline void writed(uint64_t val, uintptr_t addr)
-{
-	asm volatile("sd %0, 0(%1)"
-		     :
-		     : "r"(val), "r"((volatile uint64_t *)addr)
-		     : "memory");
-}
+// static inline void writed(uint64_t val, uintptr_t addr)
+// {
+// 	asm volatile("sd %0, 0(%1)"
+// 		     :
+// 		     : "r"(val), "r"((volatile uint64_t *)addr)
+// 		     : "memory");
+// }
 
 /* generic I/O read */
 static inline uint8_t readb(const uintptr_t addr)
@@ -93,14 +93,19 @@ static inline uint32_t readw(const uintptr_t addr)
 	return val;
 }
 
-static inline uint64_t readd(const uintptr_t addr)
-{
-	uint64_t val;
+// static inline uint64_t readd(const uintptr_t addr)
+// {
+// 	uint64_t val;
 
-	asm volatile("ld %0, 0(%1)"
-		     : "=r"(val)
-		     : "r"((const volatile uint64_t *)addr)
-		     : "memory");
-	return val;
+// 	asm volatile("ld %0, 0(%1)"
+// 		     : "=r"(val)
+// 		     : "r"((const volatile uint64_t *)addr)
+// 		     : "memory");
+// 	return val;
+// }
+
+static inline void fence() {
+	asm volatile("" : : : "memory");
 }
+
 #endif
